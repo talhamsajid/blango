084a6a223163ff9cea07d4c22e2e354f2f3c88c4
Talha Munawar <talhamsajid@gmail.com>
2024-03-14 17:12:48 +0000

    Custom template TAGS

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.auth import get_user_model
 from django.utils.html import format_html
+from blog.models import Post
 
 user_model = get_user_model()
 register = template.Library()
@@ -27,3 +28,25 @@ def author_details(author, current_user=None):
         suffix = ""
 
     return format_html('{}{}{}', prefix, name, suffix)
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+  posts = Post.objects.exclude(pk=post.pk)[:5]
+  return {"title" : "Recent Post", "posts" : posts}
+
+
+@register.simple_tag
+def row(extra_classes=""):
+    return format_html('<div class="row {}">', extra_classes)
+
+@register.simple_tag
+def endrow():
+    return format_html("</div>")
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html("</div>")
